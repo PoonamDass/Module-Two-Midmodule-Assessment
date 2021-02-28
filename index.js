c402b0e6ae1626d7a799d265f8c2737af85f210a
poonam <poonam398@gmail.com>
2021-02-28 11:33:06 -0500

    use innerHTMl to display dices

---
+++ index.js
@@ -0,0 +1,11 @@
+const dices = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"]
+
+const showRoll = document.querySelector("#show-rolls")
+showRoll.innerHTML = dices[1]
+
+const generateRoll = () =>{
+    const numberOfDice = document.querySelector("#number-of-dice")
+    const rollDiceBtn = document.querySelector("#roll-dice-btn")
+
+
+}
