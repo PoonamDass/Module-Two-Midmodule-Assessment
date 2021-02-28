34980f8ba3df23ccba695d3020683bcdaaa4cf6e
poonam <poonam398@gmail.com>
2021-02-28 11:56:59 -0500

    created an array of objects for dices

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const dices = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"]
-
+//const dices = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"]
+const rollDiceBtn = document.querySelector("#roll-dice-btn")
+const numberOfDice = document.querySelector("#number-of-dice")
 const showRoll = document.querySelector("#show-rolls")
-showRoll.innerHTML = dices[1]
+
+const dices =[{DiceNum:1,Dice: "&#9856"}, {DiceNum:2, Dice:"&#9857"}, {DiceNum:3, Dice:"&#9858"}, {DiceNum:4,Dice: "&#9859"}, {DiceNum:5, Dice:"&#9860"}, {DiceNum:6, Dice:"&#9861"}]
 
 const generateRoll = () =>{
-    const numberOfDice = document.querySelector("#number-of-dice")
-    const rollDiceBtn = document.querySelector("#roll-dice-btn")
+    showRoll.innerHTML = ""
+
+    for(let i=0; i<numberOfDice.value; i++){
+        let random= dices[Math.floor(Math.random() * dices.length)]
+        showRoll.innerHTML += random["Dice"]
+    }
 
 
 }
+
+
+rollDiceBtn.addEventListener("click", generateRoll)
